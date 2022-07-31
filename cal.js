be6f1601e681272bef1e92377bf9f35a2a226e73
Mariam <adesinamariam7@gmail.com>
2022-08-01 01:35:48 +0200

    committed

---
+++ cal.js
@@ -0,0 +1,63 @@
+
+function getclear(){
+    document.getElementById('userinput').value =' '
+}
+function getplusminus(){
+    document.getElementById('userinput').value ='+/-'
+}
+function getpercent(){
+    document.getElementById('userinput').value +='%'
+}
+function getdivide(){
+    document.getElementById('userinput').value +='/'
+}
+function getseven(){
+    document.getElementById('userinput').value +='7'
+}
+function geteight(){
+    document.getElementById('userinput').value +='8'
+}
+function getnine(){
+    document.getElementById('userinput').value +='9'
+}
+function gettimes(){
+    document.getElementById('userinput').value +='*'
+}
+function getfour(){
+    document.getElementById('userinput').value +='4'
+}
+function getfive(){
+    document.getElementById('userinput').value +='5'
+}
+function getsix(){
+    document.getElementById('userinput').value +='6'
+}
+function getone(){
+    document.getElementById('userinput').value +='1'
+}
+function gettwo(){
+    document.getElementById('userinput').value +='2'
+}
+function getthree(){
+    document.getElementById('userinput').value +='3'
+}
+function getplus() {
+    document.getElementById('userinput').value +='+'
+}
+function getminus(){
+    document.getElementById('userinput').value +='-'
+}
+function getzero(){
+    document.getElementById('userinput').value +='0'
+}
+function getpoint(){
+    document.getElementById('userinput').value +='.'
+}
+function getEqual(){
+    document.getElementById('userinput').value =eval(document.getElementById('userinput').value)
+}
+
+// function getOff(){
+//     document.getElementById('userinput').value =disabled=true(document.getElementById('userinput').value)
+// }
+
